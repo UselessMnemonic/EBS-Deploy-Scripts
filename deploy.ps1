64c2045454cb0f923d9d8ec2c10a999d0cf03ac6
Christopher Madrigal <c4madrig@ucsd.edu>
2021-04-01 23:41:44 -0700

    Update deploy.ps1
    make it pretty

--- deploy.ps1
+++ deploy.ps1
@@ -13,13 +13,13 @@ param (
     [string]$warPath,
 
     [Parameter(Mandatory=$false)]
-    [string]$version ='v'+(Get-Date -Format yyyyMMddHHmm)
+    [string]$version = "v$(Get-Date -Format yyyyMMddHHmm)"
 )
 
 # Build new deployment
-[string]$warName = $appName+'-'+$version+'.war'
+[string]$warName = "$appName-$version.war"
 aws s3 cp $warPath s3://$bucket/$warName
-aws elasticbeanstalk create-application-version --application-name $appName --version-label $version  --source-bundle S3Bucket=$bucket,S3Key=$warName
+aws elasticbeanstalk create-application-version --application-name $appName --version-label $version --source-bundle S3Bucket=$bucket,S3Key=$warName
 aws elasticbeanstalk update-environment --environment-name $envName --version-label $version
 
 exit
