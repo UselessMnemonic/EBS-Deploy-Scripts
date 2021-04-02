05b3c7aebf01b5d6ec012b229274a185518dfb33
Christopher Madrigal <c4madrig@ucsd.edu>
2021-04-01 23:23:09 -0700

    Upload WAR files to EBS Instance
    With help from https://github.com/thekosmix/AWS-Deploy/blob/master/deployOnProd.sh

---
+++ deploy.ps1
@@ -0,0 +1,25 @@
+# check for environment
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$bucket,
+
+    [Parameter(Mandatory=$true)]
+    [string]$appName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$envName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$warPath,
+
+    [Parameter(Mandatory=$false)]
+    [string]$version ='v'+(Get-Date -Format yyyyMMddHHmm)
+)
+
+# Build new deployment
+[string]$warName = $appName+'-'+$version+'.war'
+aws s3 cp $warPath s3://$bucket/$warName
+aws elasticbeanstalk create-application-version --application-name $appName --version-label $version  --source-bundle S3Bucket=$bucket,S3Key=$warName
+aws elasticbeanstalk update-environment --environment-name $envName --version-label $version
+
+exit
